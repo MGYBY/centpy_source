c121b6efe0f7cb37c40ef07500229afabf4a8bf6
BYY <53412286+MGYBY@users.noreply.github.com>
2022-01-27 22:10:06 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@ from .parameters import Pars1d, Pars2d
 from .equations import Equation1d, Equation2d
 from .solver1d import Solver1d
 from .solver1dSWE import Solver1dSWE
+from .solver1dLM import Solver1dLM
 from .solver2d import Solver2d
