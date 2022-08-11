43de35f4336f797f82d9ce09c56d3a5eb6b84ef2
BYY <53412286+MGYBY@users.noreply.github.com>
2022-08-11 10:53:04 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .parameters import Pars1d, Pars2d
+from .equations import Equation1d, Equation2d
+from .solver1d import Solver1d
+from .solver1dSWE import Solver1dSWE
+from .solver1dLM import Solver1dLM
+from .solver1dNMDIM import Solver1dNMDIM
+from .solver1dNM import Solver1dNM
+from .solver2d import Solver2d
